0a68b0e21b983151ad48671354a5ef7719d7f90a
Jordan Marr <JordanMarr@users.noreply.github.com>
2022-07-02 11:17:37 -0400

    Renamed paths and added easier cmd access for "Pack" and "PackNoTests".

--- Template/Build/Program.fs
+++ Template/Build/Program.fs
@@ -13,11 +13,11 @@ Setup.context()
 
 let path xs = Path.Combine(Array.ofList xs)
 let solutionRoot = Files.findParent __SOURCE_DIRECTORY__ "Fullstack.sln";
-let server = path [ solutionRoot; "WebApi" ]
-let client =  path [ solutionRoot; "WebLit" ]
-let serverTests = path [ solutionRoot; "WebApi.Tests" ]
-let clientTests = path [ solutionRoot; "WebLit.Tests" ]
-let clientDist = path [ client; "dist" ]
+let webApi = path [ solutionRoot; "WebApi" ]
+let webLit =  path [ solutionRoot; "WebLit" ]
+let webApiTests = path [ solutionRoot; "WebApi.Tests" ]
+let webLitTests = path [ solutionRoot; "WebLit.Tests" ]
+let webLitDist = path [ webLit; "dist" ]
 let dist = path [ solutionRoot; "dist" ]
 let clientOutput = path [ dist; "wwwroot" ]
 
@@ -25,72 +25,72 @@ Target.create "Clean" <| fun _ ->
     // sometimes files are locked by VS for a bit, retry again until they can be deleted
     Retry.retry 5 <| fun _ -> Shell.deleteDirs [
         dist
-        path [ server; "bin" ]
-        path [ server; "obj" ]
-        path [ serverTests; "bin" ]
-        path [ serverTests; "obj" ]
-        path [ client; "bin" ]
-        path [ client; "obj" ]
-        path [ client; ".fable" ]
-        path [ clientTests; "bin" ]
-        path [ clientTests; "obj" ]
-        path [ clientTests; ".fable" ]
+        path [ webApi; "bin" ]
+        path [ webApi; "obj" ]
+        path [ webApiTests; "bin" ]
+        path [ webApiTests; "obj" ]
+        path [ webLit; "bin" ]
+        path [ webLit; "obj" ]
+        path [ webLit; ".fable" ]
+        path [ webLitTests; "bin" ]
+        path [ webLitTests; "obj" ]
+        path [ webLitTests; ".fable" ]
     ]
 
 Target.create "RestoreServer" <| fun _ ->
     Retry.retry 5 <| fun _ ->
-        let exitCode = Shell.Exec(Tools.dotnet, "restore", server)
+        let exitCode = Shell.Exec(Tools.dotnet, "restore", webApi)
         if exitCode <> 0 then failwith "Could restore packages in the server project"
 
 Target.create "Server" <| fun _ ->
     Retry.retry 5 <| fun _ ->
-        let exitCode = Shell.Exec(Tools.dotnet, "build --configuration Release", server)
+        let exitCode = Shell.Exec(Tools.dotnet, "build --configuration Release", webApi)
         if exitCode <> 0 then failwith "Could not build the server project"
 
 Target.create "ServerTests" <| fun _ ->
-    let exitCode = Shell.Exec(Tools.dotnet, "run --configuration Release", serverTests)
+    let exitCode = Shell.Exec(Tools.dotnet, "run --configuration Release", webApiTests)
     if exitCode <> 0 then failwith "Failed while while running server tests"
 
 Target.create "RestoreClient" <| fun _ ->
-    let exitCode = Shell.Exec(Tools.npm, "install", client)
+    let exitCode = Shell.Exec(Tools.npm, "install", webLit)
     if exitCode <> 0 then failwith "failed to run `npm install` in the client directory"
 
 Target.create "Client" <| fun _ ->
-    let exitCode = Shell.Exec(Tools.npm, "run build", client)
+    let exitCode = Shell.Exec(Tools.npm, "run build", webLit)
     if exitCode <> 0 then failwith "Failed to build client"
 
 Target.create "ClientTests" <| fun _ ->
-    let exitCode = Shell.Exec(Tools.npm, "test", client)
+    let exitCode = Shell.Exec(Tools.npm, "test", webLit)
     if exitCode <> 0 then failwith "Client tests did not pass"
 
 Target.create "HeadlessBrowserTests" <| fun _ ->
-    Shell.cleanDir clientDist
-    let exitCode = Shell.Exec(Tools.npm, "run build:test", client)
+    Shell.cleanDir webLitDist
+    let exitCode = Shell.Exec(Tools.npm, "run build:test", webLit)
     if exitCode <> 0 then
         failwith "Failed to build tests project"
     else
-        let testResults = Async.RunSynchronously(Puppeteer.runTests clientDist)
+        let testResults = Async.RunSynchronously(Puppeteer.runTests webLitDist)
         if testResults <> 0 then failwith "Some tests failed"
 
 Target.create "LiveClientTests" <| fun _ ->
-    let exitCode = Shell.Exec(Tools.npm, "run test:live", client)
+    let exitCode = Shell.Exec(Tools.npm, "run test:live", webLit)
     if exitCode <> 0 then failwith "Failed to run client tests"
 
 Target.create "Pack" <| fun _ ->
-    match Shell.Exec(Tools.dotnet, sprintf "publish --configuration Release --output %s" dist, server) with
+    match Shell.Exec(Tools.dotnet, sprintf "publish --configuration Release --output %s" dist, webApi) with
     | 0 ->
-        let exitCode = Shell.Exec(Tools.npm, "run build", client)
+        let exitCode = Shell.Exec(Tools.npm, "run build", webLit)
         if exitCode <> 0 then failwith "Failed to build client"
-        Shell.copyDir clientOutput clientDist (fun file -> true)
+        Shell.copyDir clientOutput webLitDist (fun file -> true)
     | n ->
         failwith "Failed to publish server project"
 
 Target.create "PackNoTests" <| fun _ ->
-    match Shell.Exec(Tools.dotnet, sprintf "publish --configuration Release --output %s" dist, server) with
+    match Shell.Exec(Tools.dotnet, sprintf "publish --configuration Release --output %s" dist, webApi) with
     | 0 ->
-        match Shell.Exec(Tools.npm, "run build", client) with
+        match Shell.Exec(Tools.npm, "run build", webLit) with
         | 0 ->
-            Shell.copyDir clientOutput clientDist (fun file -> true)
+            Shell.copyDir clientOutput webLitDist (fun file -> true)
         | _ ->
             failwith "Failed to build the client project"
     | _ ->
@@ -109,6 +109,7 @@ let dependencies = [
     "RestoreClient" ==> "ClientTests"
     "ServerTests" ==> "Pack"
     "ClientTests" ==> "Pack"
+    "Server" ==> "Client" ==> "PackNoTests"
     "RestoreClient" ==> "PackNoTests"
 ]
 
@@ -119,6 +120,8 @@ let main (args: string[]) =
         match args with
         | [| "RunDefaultOr" |] -> Target.runOrDefault "Default"
         | [| "RunDefaultOr"; target |] -> Target.runOrDefault target
+        | [| "PackNoTests" |] -> Target.runOrDefault "PackNoTests"
+        | [| "Pack" |] -> Target.runOrDefault "Pack"
         | manyArguments ->
             Console.Write("[Interactive Mode] Run build target: ")
             let target = Console.ReadLine()
