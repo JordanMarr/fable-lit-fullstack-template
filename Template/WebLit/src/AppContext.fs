5701a4415bc758e98f09f114f8ffd3ceb9c5eae7
Jordan Marr <JordanMarr@users.noreply.github.com>
2022-08-23 12:36:36 -0400

    Added `useStore` hook.

---
+++ Template/WebLit/src/AppContext.fs
@@ -0,0 +1,23 @@
+﻿module WebLit.AppContext
+
+open ElmishStore
+
+type Model = 
+    {
+        Username: string
+    }
+
+let init () = 
+    { Username = "Anonymous" }, Cmd.none
+
+type Msg = 
+    | SetUsername of string
+
+let update (msg: Msg) (model: Model) =
+    match msg with
+    | SetUsername username -> 
+        { model with Username = username }, Cmd.none
+
+let dispose _ = ()
+
+let store, dispatch = Store.makeElmish init update dispose ()
